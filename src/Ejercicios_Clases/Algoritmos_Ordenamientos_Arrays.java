a2c28e3bed7e6159a2e8945cd1cabdf59c4cf47e
TitoMunozAbreu <munoztitojose@gmail.com>
2022-12-06 20:06:44 +0100

    DAM: Repositorio clase de programacion primer año.

--- src/Ejercicios_Clases/Algoritmos_Ordenamientos_Arrays.java
+++ src/Ejercicios_Clases/Algoritmos_Ordenamientos_Arrays.java
@@ -4,25 +4,32 @@ import java.util.Arrays;
 
 public class Algoritmos_Ordenamientos_Arrays {
     public static void main(String[] args) {
-        int [] n = {5, 2, 1, 8, 3, 9, 7};
+        int [] arreglo = {5, 2, 1, 8, 3, 9, 7};
+
         //mostar por pantalla el array sin ordenar
-        System.out.println("Array SIN ordenar: " + Arrays.toString(n));
-        System.out.println();
+        System.out.println("Array SIN ordenar: " + Arrays.toString(arreglo));
 
-        //Le pasamos el array, el segundo parametro es cero por defecto
-        //el tercer parametro es el ultimo indice del array
+        //mostrar array ordenado con el metodo quicksort
+        //System.out.println("Metodo quicksort - array rrdenado: " + Arrays.toString(quicksort(arreglo,0,arreglo.length-1)));
 
-        System.out.println("Array Ordenado: " + Arrays.toString(quicksort(n,0,n.length-1)));
-        //mostrar por pantalla el array ordenado
+        //mostrar array ordenado con el metodo burbuja
+        System.out.println("Metodo burbuja - array ordenado: " + Arrays.toString(burbuja(arreglo)));
 
 
     }
 
+    /**
+     *   METODO SEGUN EL ALGORITMO QUICKSORT
+     * @param arreglo Le pasamos el array que se quiere ordenar
+     * @param primero Por defecto es el primer indice del array 0
+     * @param ultimo Utilizamos el ultimo indice del  array -1
+     * @return Devuelve el array ordenado
+     */
     public static int[] quicksort(int[]arreglo, int primero, int ultimo){
         //Incializamos 4 variables
         int i, j, auxiliar, pivote;
-        i = primero; // indice i toma el valor de primero
-        j = ultimo; // indice j toma el valor de ultimo
+        i = primero; // indice i toma el valor del primer indice
+        j = ultimo; // indice j toma el valor del ultimo indice
         pivote = arreglo[(primero+ultimo)/2]; //el pivote para compararlo con la posicion del primero y el ultimo
 
         do {//Do mientras primero sea menor igual a ultimo
@@ -58,6 +65,40 @@ public class Algoritmos_Ordenamientos_Arrays {
         return arreglo;
 
     }
+
+    /**
+     * METODO SEGUN EL ALGORITMO BURBURJA
+     * @param arreglo Le array que se quiere ordenar.
+     * @return Devuelve el array ordenado
+     */
+    public static int[] burbuja(int[] arreglo){
+        //Variable para utilizar en el intercambio
+        int temporal;
+
+        //Recorrer el array
+        for (int i = 0; i < arreglo.length; i++) {
+            for(int j = i+1; j < arreglo.length; j++){
+                //Comprobar si la primera posicion del array es mayor a la siguiente posicion.
+                if(arreglo[i] > arreglo[j]){
+                    //Realizar el intercambio
+                    temporal = arreglo[i]; // guardamos en auxiliar el valor de la primera posicion.
+                    arreglo[i] = arreglo[j]; //actualizamos el valor de la primera posicion con el menor valor de la segunda posicion
+                    arreglo[j] = temporal; // actualizamos el valor de la segunda posicion con el valor mayor de la primera posicion
+                }
+            }
+        }
+        return arreglo;
+    }
+
+    /**
+     * METODO SEGUN EL ALGORITMO SHELL
+     * @return Devuelve el array ordenado
+     */
+    public static int[] shell(int[] arreglo){
+        return arreglo;
+
+    }
+
 
 
 
