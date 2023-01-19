f5c53479fca41c9eede2adda8defe58bf7d1a8e7
titomunoz <munoztitojose@gmail.com>
2023-01-19 13:14:55 +0100

    DAM: Ejercicios de programacion orientada a objetos mejoras a la clase Ficha.java

--- src/Ejercicios_OPO_FichaAutoEscuela/Ficha.java
+++ src/Ejercicios_OPO_FichaAutoEscuela/Ficha.java
@@ -8,57 +8,84 @@ public class Ficha {
     private Alumno[] alumnos;
     private Coche[] coches;
 
+    int contador_p = 0;
+    int contador_a = 0;
+    int contador_c = 0;
+    int n = 2;
+
     public Ficha(){
-        this.profesores = new Profesor[1];
-        this.alumnos = new Alumno[1];
-        this.coches = new Coche[1];
+        this.profesores = new Profesor[n];
+        this.alumnos = new Alumno[n];
+        this.coches = new Coche[n];
     }
 
     public void addProfesor (Profesor p){
-        for (int i = 0; i < profesores.length; i++) {
-            profesores[i] = p;
+        if(contador_p < n){
+            for (int i = contador_p; i < profesores.length;) {
+                profesores[i] = p;
+                contador_p++;
+                System.out.println((i+1) + ": Profesor agregado al fichero exitosamente");
+                break;
+            }
+        }else{
+            System.out.println("NO es posible contratar mas profesores");
         }
-        System.out.println("Profesor agregado al fichero exitosamente");
         System.out.println();
     }
 
     public void addAlumno (Alumno a){
-        for (int i = 0; i < alumnos.length; i++) {
-            alumnos[i] = a;
+        if(contador_a < n) {
+            for (int i = contador_a; i < alumnos.length; i++) {
+                alumnos[i] = a;
+                contador_a++;
+                System.out.println((i+1) + ":Alumno agregado al fichero exitosamente");
+                break;
+            }
+        }else{
+            System.out.println("NO es posible contratar mas alumnos");
         }
-        System.out.println("Alumno agregado al fichero exitosamente");
         System.out.println();
     }
 
     public void addCoche (Coche c){
-        for (int i = 0; i < coches.length; i++) {
+        if(contador_c < n) {
+            for (int i = contador_c; i < coches.length;) {
             coches[i] = c;
+            contador_c++;
+            System.out.println((i+1) + ":Coche agregado al fichero exitosamente");
+            break;
+            }
+        }else{
+            System.out.println("NO es posible comprar mas coches");
         }
-        System.out.println("Coche agregado al fichero exitosamente");
+
         System.out.println();
     }
 
     public void getProfesor(String dni){
-        for (int i = 0; i < profesores.length; i++) {
-            if(profesores[i].getDNI().equals(dni)){
-                System.out.println(profesores[i].toString());
+        for (Profesor profesor : profesores) {
+            if (profesor.getDNI().equals(dni)) {
+                System.out.println(profesor.toString());
             }
+            break;
         }
     }
 
     public void getAlumno(String dni){
-        for (int i = 0; i < alumnos.length; i++) {
+        for (int i = 0; i < alumnos.length;) {
             if(alumnos[i].getDNI().equals(dni)){
                 System.out.println(alumnos[i].toString());
             }
+            break;
         }
     }
 
     public void getcoche(String m){
-        for (int i = 0; i < coches.length; i++) {
+        for (int i = 0; i < coches.length;) {
             if(coches[i].getMatricula().equals(m)){
                 System.out.println(coches[i].toString());
             }
+            break;
         }
     }
 
--- src/Ejercicios_OPO_FichaAutoEscuela/Test.java
+++ src/Ejercicios_OPO_FichaAutoEscuela/Test.java
@@ -5,13 +5,26 @@ public class Test {
         Profesor p = new Profesor("Conchita e", "Mango", "1234X", 1350.85,"Papaya");
         Alumno a = new Alumno("Pepe", "Martin", "1235Y", 'B');
         Coche c = new Coche("456-XMS", "seat", "ibiza");
+
+        Profesor p1 = new Profesor("Platano", "Melon", "6589X", 1050.50,"Papaya");
+        Alumno a1 = new Alumno("Pitaya", "Moraa", "5515Y", 'B');
+        Coche c1 = new Coche("697-DFE", "ford", "fiesta");
+
         Ficha f = new Ficha();
 
         f.addProfesor(p);
         f.addAlumno(a);
         f.addCoche(c);
 
+        f.addProfesor(p1);
+        f.addAlumno(a1);
+        f.addCoche(c1);
+
         System.out.println(f);
+
+        f.addProfesor(p1);
+
+
 
     }
 }
